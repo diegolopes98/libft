b149046e261b2ff79c52e19686a6b753c463c3db
Diego Pereira Lopes <dieperei@student.42sp.org.br>
2022-05-31 22:49:35 +0200

    fix: ft_memset implementation

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: dieperei <dieperei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/31 22:12:05 by dieperei          #+#    #+#             */
-/*   Updated: 2022/05/31 22:28:53 by dieperei         ###   ########.fr       */
+/*   Updated: 2022/05/31 22:49:10 by dieperei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,13 @@
 
 void	*ft_memset(void *s, int c, size_t n)
 {
+	unsigned char	*ptr;
+
+	ptr = s;
 	while (n > 0)
 	{
-		*s = c;
-		s++;
+		*ptr = c;
+		ptr++;
 		n--;
 	}		
 	return (s);
