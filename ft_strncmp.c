902fc7669bf1d40859a3315db9f688b3ddcbc420
Diego Pereira Lopes <dieperei@student.42sp.org.br>
2022-06-01 18:35:42 +0200

    fix: norminette ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,25 +6,26 @@
 /*   By: dieperei <dieperei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/01 00:54:06 by dieperei          #+#    #+#             */
-/*   Updated: 2022/06/01 18:05:21 by dieperei         ###   ########.fr       */
+/*   Updated: 2022/06/01 18:35:07 by dieperei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int ft_strncmp(const char *s1, const char *s2, size_t n)
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	unsigned char c1;
-	unsigned char c2;
+	unsigned char	c1;
+	unsigned char	c2;
 
-	while (n) {
+	while (n)
+	{
 		c1 = *s1++;
 		c2 = *s2++;
 		if (c1 != c2)
-			return c1 - c2;
+			return (c1 - c2);
 		if (!c1)
 			n = 0;
 		n--;
 	}
-	return 0;
+	return (0);
 }
