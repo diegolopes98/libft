a9ba884e2cba4250cbe4361587c9c31196421699
Diego Pereira Lopes <dieperei@student.42sp.org.br>
2022-06-01 21:03:02 +0200

    feat: add ft_memmove.c

---
+++ ft_memmove.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dieperei <dieperei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/01 19:33:32 by dieperei          #+#    #+#             */
+/*   Updated: 2022/06/01 20:56:14 by dieperei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	char		*ldest;
+	char		*cdest;
+	const char	*lsrc;
+	const char	*csrc; 	
+
+	cdest = dest;
+	csrc = src;
+	if (dest < src)
+		ft_memcpy(dest, src, n);
+	else
+	{
+		ldest = dest + (n - 1);
+		lsrc = src + (n - 1);
+		while (n--)
+			*ldest-- = *lsrc--;
+	}
+	return (dest);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: dieperei <dieperei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/31 18:53:19 by dieperei          #+#    #+#             */
-/*   Updated: 2022/06/01 18:26:26 by dieperei         ###   ########.fr       */
+/*   Updated: 2022/06/01 21:02:29 by dieperei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,5 +33,6 @@ int		ft_tolower(int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
 
 #endif // LIBFT_H
