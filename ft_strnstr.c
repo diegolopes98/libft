cdda5ec80b50d5b1153c4b299c38e7f1e6d386a7
Diego Pereira Lopes <dieperei@student.42sp.org.br>
2022-06-01 23:15:38 +0200

    feat: add ft_strnstr.c

---
+++ ft_strnstr.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dieperei <dieperei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/01 22:08:48 by dieperei          #+#    #+#             */
+/*   Updated: 2022/06/01 23:11:26 by dieperei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *big, const char *little, size_t len)
+{
+	size_t	b;
+	size_t	l;
+
+	b = 0;
+	if (little[0] == '\0')
+		return ((char *)big);
+	while (big[b] != '\0')
+	{
+		l = 0;
+		while (big[b + l] == little[l] && (b + l) < len)
+		{
+			if (big[b + l] == '\0' && little[l] == '\0')
+				return ((char *)&big[b]);
+			l++;
+		}
+		if (little[l] == '\0')
+			return ((char *)big + l);
+		b++;
+	}
+	return (0);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: dieperei <dieperei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/31 18:53:19 by dieperei          #+#    #+#             */
-/*   Updated: 2022/06/01 21:02:29 by dieperei         ###   ########.fr       */
+/*   Updated: 2022/06/01 23:09:12 by dieperei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,5 +34,6 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
+char	*ft_strnstr(const char *big, const char *little, size_t len);
 
 #endif // LIBFT_H
