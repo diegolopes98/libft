0b24bfa72e778037b33695e0908fdb788539e5df
Diego Pereira Lopes <dieperei@student.42sp.org.br>
2022-06-01 18:07:03 +0200

    fix: ft_strncmp implementation

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,21 +6,25 @@
 /*   By: dieperei <dieperei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/01 00:54:06 by dieperei          #+#    #+#             */
-/*   Updated: 2022/06/01 06:21:35 by dieperei         ###   ########.fr       */
+/*   Updated: 2022/06/01 18:05:21 by dieperei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_strncmp(const char *s1, const char *s2, size_t n)
+int ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	int	i;
+	unsigned char c1;
+	unsigned char c2;
 
-	i = 0;
-	while (n && s1[i] != '\0' && s1[i] == s2[i])
-	{
-		i++;
+	while (n) {
+		c1 = *s1++;
+		c2 = *s2++;
+		if (c1 != c2)
+			return c1 - c2;
+		if (!c1)
+			n = 0;
 		n--;
 	}
-	return ((unsigned char) s1[i] - (unsigned char) s2[i]);
+	return 0;
 }
