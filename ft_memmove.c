59e1c2e02fdc19a4a6f554071001fdedf550f2e5
Diego Pereira Lopes <dieperei@student.42sp.org.br>
2022-06-01 21:48:09 +0200

    fix: norminette ft_memmove.c

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: dieperei <dieperei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/01 19:33:32 by dieperei          #+#    #+#             */
-/*   Updated: 2022/06/01 20:56:14 by dieperei         ###   ########.fr       */
+/*   Updated: 2022/06/01 21:47:58 by dieperei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	char		*ldest;
 	char		*cdest;
 	const char	*lsrc;
-	const char	*csrc; 	
+	const char	*csrc;
 
 	cdest = dest;
 	csrc = src;
