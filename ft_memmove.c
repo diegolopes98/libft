c5f1c2a89b1b6d50fa0f97cfdfdf3752f9c2fc7c
Diego Pereira Lopes <dieperei@student.42sp.org.br>
2022-06-06 14:49:14 +0200

    fix: removed unused variables ft_memmove.c

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: dieperei <dieperei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/01 19:33:32 by dieperei          #+#    #+#             */
-/*   Updated: 2022/06/01 21:47:58 by dieperei         ###   ########.fr       */
+/*   Updated: 2022/06/06 14:47:16 by dieperei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,8 @@
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	char		*ldest;
-	char		*cdest;
 	const char	*lsrc;
-	const char	*csrc;
 
-	cdest = dest;
-	csrc = src;
 	if (dest < src)
 		ft_memcpy(dest, src, n);
 	else
