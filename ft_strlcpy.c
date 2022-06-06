c3154f42e9105867c5368f8d82017d3b3b87568a
Diego Pereira Lopes <dieperei@student.42sp.org.br>
2022-06-06 17:45:26 +0200

    fix: return value and size check

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: dieperei <dieperei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/31 18:34:47 by dieperei          #+#    #+#             */
-/*   Updated: 2022/05/31 19:11:59 by dieperei         ###   ########.fr       */
+/*   Updated: 2022/06/06 17:29:49 by dieperei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,17 @@
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
 	size_t	i;
-	size_t	len;
+	size_t	srclen;
 
 	i = 0;
-	len = ft_strlen(src);
-	if (size < 1)
-		return (len);
+	srclen = ft_strlen(src);
+	if (!size)
+		return (srclen);
 	while (src[i] != '\0' && i < size - 1)
 	{
 		dst[i] = src[i];
 		i++;
 	}
 	dst[i] = '\0';
-	return (i);
+	return (srclen);
 }
