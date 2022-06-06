e86288220561d9c65934d410b70a9a5f93a97eb4
Diego Pereira Lopes <dieperei@student.42sp.org.br>
2022-06-06 21:15:56 +0200

    fix: removed unnecessary main and fixed norminette

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: dieperei <dieperei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/06 18:30:18 by dieperei          #+#    #+#             */
-/*   Updated: 2022/06/06 20:35:52 by dieperei         ###   ########.fr       */
+/*   Updated: 2022/06/06 21:15:31 by dieperei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	if (start >= slen)
 		return (ft_strdup(""));
 	substr = malloc(sizeof(char) * (len + 1));
-	if(!substr)
+	if (!substr)
 		return (0);
 	i = 0;
 	while (i < len && s[start + i] != '\0')
@@ -34,14 +34,4 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	}
 	substr[i] = '\0';
 	return (substr);
-}
-
-#include <stdio.h>
-
-int main(void)
-{
-	char *str = "i just want this part #############";
-	size_t size = 20;
-	char *ret = ft_substr(str, 5, size);
-	printf("%s\n", ret);
 }
