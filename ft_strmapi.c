a4e4e47e89f453db4ef686c67be8bf5a088c488d
Diego Pereira Lopes <dieperei@student.42sp.org.br>
2022-06-07 22:06:48 +0200

    refactor: add f param to safe guard in ft_strmapi

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: dieperei <dieperei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/07 21:54:17 by dieperei          #+#    #+#             */
-/*   Updated: 2022/06/07 21:54:24 by dieperei         ###   ########.fr       */
+/*   Updated: 2022/06/07 22:06:20 by dieperei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	char	*res;
 	size_t	len;
 
-	if (!s)
+	if (!s || !f)
 		return (NULL);
 	len = ft_strlen(s);
 	res = malloc(sizeof(char) * (len + 1));
