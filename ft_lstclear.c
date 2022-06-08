b9a523381a088cac58a4fc3ac769952850838dc4
Diego Pereira Lopes <dieperei@student.42sp.org.br>
2022-06-08 22:18:40 +0200

    fix: norminette ft_lstclear

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: dieperei <dieperei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/08 20:10:00 by dieperei          #+#    #+#             */
-/*   Updated: 2022/06/08 20:21:38 by dieperei         ###   ########.fr       */
+/*   Updated: 2022/06/08 22:18:18 by dieperei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ void	ft_lstclear(t_list **lst, void (*del)(void*))
 {
 	if (!lst || !del)
 		return ;
-	if ((*lst) -> next)
-		ft_lstclear(&((*lst) -> next), del);
+	if ((*lst)-> next)
+		ft_lstclear(&((*lst)-> next), del);
 	ft_lstdelone(*lst, del);
 	*lst = NULL;
 }
